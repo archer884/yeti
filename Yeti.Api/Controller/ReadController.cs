a0ae081439c35dd67b5d897e7427eb1da8d711fe
J M Archer <archer884@zoho.com>
2024-08-07 12:33:18 -0500

    Fix new and updated methods

--- Yeti.Api/Controller/ReadController.cs
+++ Yeti.Api/Controller/ReadController.cs
@@ -7,44 +7,36 @@ namespace Yeti.Api.Controller;
 
 [ApiController]
 [Route("[controller]")]
-public class ReadController(
-    ManuscriptService manuscript,
-    FragmentService fragment,
-    RecentService recentService)
+public class ReadController(ManuscriptService manuscript, FragmentService fragment, RecentService recentService)
     : ControllerBase
 {
     /// <summary>
     /// Gets the newest manuscripts.
     /// </summary>
-    [HttpGet("/new")]
-    public async Task<ActionResult<List<ManuscriptSummary>>> GetNew()
-        => await recentService.ByNew(0);
+    [HttpGet("new")]
+    public async Task<ActionResult<List<ManuscriptSummary>>> GetNew() => await recentService.ByNew(0);
 
     /// <summary>
     /// Gets the most recently updated manuscripts.
     /// </summary>
-    [HttpGet("/updated")]
-    public async Task<ActionResult<List<ManuscriptSummary>>> GetUpdated()
-        => await recentService.ByUpdated(0);
+    [HttpGet("updated")]
+    public async Task<ActionResult<List<ManuscriptSummary>>> GetUpdated() => await recentService.ByUpdated(0);
 
     /// <summary>
     /// Retrieves a fragment for rendering.
     /// </summary>
     [HttpGet("{id}")]
-    public async Task<ActionResult<FragmentDetail?>> GetFragmentDetail(long id)
-        => await fragment.GetFragmentDetail(id);
+    public async Task<ActionResult<FragmentDetail?>> GetFragmentDetail(long id) => await fragment.GetFragmentDetail(id);
 
     /// <summary>
     /// Retrieves a manuscript summary.
     /// </summary>
     [HttpGet("summary/{id}")]
-    public async Task<ActionResult<ManuscriptSummary?>> Get(long id)
-        => await manuscript.ManuscriptSummary(id);
+    public async Task<ActionResult<ManuscriptSummary?>> Get(long id) => await manuscript.ManuscriptSummary(id);
 
     /// <summary>
     /// Retrieves a summary of fragments for a manuscript.
     /// </summary>
     [HttpGet("listing/{id}")]
-    public async Task<ActionResult<List<FragmentSummary>>> GetFragments(long id)
-        => await manuscript.FragmentSummaries(id);
+    public async Task<ActionResult<List<FragmentSummary>>> GetFragments(long id) => await manuscript.FragmentSummaries(id);
 }
